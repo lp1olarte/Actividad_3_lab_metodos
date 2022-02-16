198a1a12c2c81b3f06ee826c75ee1667eb0e4819
lp1olarte <89936643+lp1olarte@users.noreply.github.com>
2022-02-16 14:31:29 -0500

    Add files via upload

---
+++ primer_programa.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string> 
+using namespace std;//se puede quitar el acronimo de la libreria std
+int main(){
+cout<<"este es mi primer programa"<<endl;  //similar a np.cos()   
+int a=4;
+float b=6.0;
+double c=1e-6;
+float k;
+k=1;
+cout<<"b="<<b<<endl;
+int s;
+cout<<"ingrese el valor entero: ";cin>>s;
+const char carac[]=" jsjjfjfj fff"; 
+cout<<carac<<endl;
+string g="jajaja jejej";
+double d;
+d=10;
+cout<<d+1<<endl;
+//Numeros binarios hexadecimales
+int v= 0b11;
+int w= 0xa;
+cout<<"Numero binario"<<v<<endl;
+cout<<"Numero hexadecimal"<<w<<endl;
+return 0;
+}
